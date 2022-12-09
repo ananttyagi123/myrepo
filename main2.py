0bc51e940d0fd8d837b31f7cd2735d929ad4a4f3
ananttyagi123 <120208520+ananttyagi123@users.noreply.github.com>
2022-12-09 23:35:44 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,13 @@
+#membership operator
+#in
+#integer ans float doesnot work
+#only string
+x=input("enter the string")
+y=input("enter the secont string")
+print(x in y)
+x=input("enter the string 1")
+y=input("enter the second string 2")
+print(x in y)
+#list ouputs
+w=["apple"]
+print("apple" in w)
